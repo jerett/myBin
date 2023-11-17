c8d14b2918dea94bf2bc4190870f75516bb39fea
jerett <wj.jiang@outlook.com>
2023-11-17 16:27:15 +0800

    add install.sh

---
+++ config_tmux.sh
@@ -0,0 +1,10 @@
+
+
+
+# for tmux 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp .tmux.conf ~/.tmux.conf
+
+# type this in terminal if tmux is already running
+tmux source ~/.tmux.conf
+
--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-BASEDIR=$(cd `dirname $0`; pwd)
-echo $BASEDIR
-ln -s $BASEDIR/.vimrc $BASEDIR/.zshrc $BASEDIR/.ideavimrc $HOME
+# BASEDIR=$(cd `dirname $0`; pwd)
+# echo $BASEDIR
+# ln -s $BASEDIR/.vimrc $BASEDIR/.zshrc $BASEDIR/.ideavimrc $BASEDIR/.tmux.conf $HOME
+
+sh config_tmux.sh
+sh config_zsh.sh
+sh config_vim.sh   # you need to enter vim, and 
